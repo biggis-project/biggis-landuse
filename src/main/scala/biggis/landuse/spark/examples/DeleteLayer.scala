022520be46e86377f9987e40f6c808c3af416763
Viliam Simko <viliam.simko@gmail.com>
2016-10-05 10:25:12 +0200

    (example) added DeleteLayer

---
+++ src/main/scala/biggis/landuse/spark/examples/DeleteLayer.scala
@@ -0,0 +1,25 @@
+package biggis.landuse.spark.examples
+
+import com.typesafe.scalalogging.slf4j.StrictLogging
+import geotrellis.spark.io.file.{FileAttributeStore, FileLayerDeleter}
+
+/**
+  * Created by Viliam Simko on 10/4/16.
+  */
+object DeleteLayer extends StrictLogging {
+  def main(args: Array[String]): Unit = {
+    val Array(catalogPath, layerName) = args
+    DeleteLayer(layerName)(catalogPath)
+  }
+
+  def apply(layerName: String)(implicit catalogPath: String): Unit = {
+    logger info s"Deleting layer $layerName including all zoom levels in catalog $catalogPath ..."
+
+    val attributeStore = FileAttributeStore(catalogPath)
+    val deleter = FileLayerDeleter(attributeStore)
+    attributeStore.layerIds.filter(_.name == layerName).foreach(deleter.delete)
+
+    logger info "done"
+  }
+
+}
