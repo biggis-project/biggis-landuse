ee3cac9210f3d6d6469641ecbd2587b2b05b45a5
Viliam Simko <viliam.simko@gmail.com>
2016-11-04 10:33:46 +0100

    added tile size and other common constants to the Utils object

--- src/main/scala/biggis/landuse/spark/examples/Utils.scala
+++ src/main/scala/biggis/landuse/spark/examples/Utils.scala
@@ -1,11 +1,21 @@
 package biggis.landuse.spark.examples
 
+import com.typesafe.scalalogging.slf4j.LazyLogging
+import geotrellis.raster.histogram.Histogram
+import geotrellis.raster.io.HistogramDoubleFormat
+import geotrellis.raster.resample.Bilinear
+import geotrellis.spark.LayerId
+import geotrellis.spark.io.AttributeStore
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Created by Viliam Simko on 2016-11-04
   */
-object Utils {
+object Utils extends LazyLogging {
+
+  val TILE_SIZE = 256
+  val RDD_PARTITIONS = 32
+  val RESAMPLING_METHOD = Bilinear
 
   def initSparkContext(): SparkContext = {
     val sparkConf = new SparkConf()
@@ -20,4 +30,11 @@ object Utils {
 
     return new SparkContext(sparkConf)
   }
+
+  def writeHistogram(attributeStore: AttributeStore, layerName: String, histogram: Histogram[Double]): Unit =  {
+    logger debug s"Writing histogram of layer '$layerName' to attribute store as 'histogramData' for zoom level 0"
+    attributeStore.write(
+      LayerId(layerName, 0), "histogramData", histogram)
+  }
+
 }
