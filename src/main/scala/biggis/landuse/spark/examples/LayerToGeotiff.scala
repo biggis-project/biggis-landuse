3658a638ad59fdd9fdf12990fd65dfd124ee6cf2
Adrian Klink <Adrian.Klink@eftas.com>
2016-11-21 14:38:38 +0100

    LayerToGeotiff example (Singleband only)

---
+++ src/main/scala/biggis/landuse/spark/examples/LayerToGeotiff.scala
@@ -0,0 +1,66 @@
+package biggis.landuse.spark.examples
+
+import com.typesafe.scalalogging.slf4j.LazyLogging
+import geotrellis.raster.{Tile, withTileMethods}
+import geotrellis.spark.{LayerId, Metadata, SpatialKey, TileLayerMetadata}
+import geotrellis.spark.io.file.{FileAttributeStore, FileLayerManager, FileLayerReader, FileLayerWriter}
+import geotrellis.spark.io.{SpatialKeyFormat, spatialKeyAvroFormat, tileLayerMetadataFormat, tileUnionCodec}
+import geotrellis.util._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+import geotrellis.raster.{io => _, _}
+import geotrellis.raster.io.geotiff._
+import geotrellis.spark.io.hadoop.{HadoopAttributeStore, HadoopLayerReader}
+import geotrellis.spark.stitch._
+import geotrellis.spark.{io => _, _}
+import org.apache.hadoop.fs.Path
+
+// https://github.com/geotrellis/geotrellis/blob/master/docs/spark/spark-examples.md
+
+object LayerToGeotiff extends LazyLogging{
+  def main(args: Array[String]): Unit = {
+    try {
+      val Array(layerName, outputPath, catalogPath) = args
+      LayerToGeotiff(layerName, outputPath)(catalogPath)
+    } catch {
+      case _: MatchError => println("Run as: layerName outputPath /path/to/catalog")
+    }
+  }
+
+  def apply(layerName: String, outputPath: String)(implicit catalogPath: String): Unit = {
+    logger info s"Writing layer '$layerName' in catalog '$catalogPath' to '$outputPath'"
+
+    implicit val sc = Utils.initSparkContext
+
+    val catalogPathHdfs = new Path(catalogPath)
+    val attributeStore = HadoopAttributeStore(catalogPathHdfs)
+    val layerReader = HadoopLayerReader(attributeStore)
+
+    val zoomsOfLayer = attributeStore.layerIds filter (_.name == layerName)
+    if(zoomsOfLayer.isEmpty) {
+      logger info s"Layer '$layerName' not found in the catalog '$catalogPath'"
+      return
+    }
+
+    val srcLayerId = zoomsOfLayer.sortBy(_.zoom).last
+    logger debug s"The following layerId will be used: $srcLayerId"
+
+    val inputRdd:RDD[(SpatialKey, Tile)] with Metadata[TileLayerMetadata[SpatialKey]] = layerReader
+      .read[SpatialKey, Tile, TileLayerMetadata[SpatialKey]](srcLayerId)
+
+    val metadata = inputRdd.metadata
+
+    val tiled: RDD[(SpatialKey, Tile)] = inputRdd.distinct()
+
+    val tile: Tile = tiled.stitch()
+
+    val crs = metadata.crs
+    val raster: Raster[Tile] = tile.reproject(metadata.extent, metadata.crs, metadata.crs)
+    GeoTiff(raster, crs).write(outputPath)
+
+    sc.stop()
+    logger debug "Spark context stopped"
+
+    logger info "done."
+  }
+}
