c25cb3771190dd3ed7b18ddfbc55125243027cdb
Adrian Klink <Adrian.Klink@eftas.com>
2016-12-01 14:56:39 +0100

    UtilsSVM (prototype): convert MultibandTile -> LabelPoint, save as LibSVM file

---
+++ src/main/scala/biggis/landuse/spark/examples/UtilsSVM.scala
@@ -0,0 +1,47 @@
+package biggis.landuse.spark.examples
+
+import geotrellis.raster.MultibandTile
+import geotrellis.spark.SpatialKey
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.rdd.RDD
+
+/**
+  * Created by ak on 01.12.2016.
+  */
+object UtilsSVM {
+
+  def MultibandTile2LabelPoint (rdd : RDD[(SpatialKey, MultibandTile)]): RDD[LabeledPoint] = {
+    // ToDo: Select only pixels within training data
+    val tile: MultibandTile = rdd
+      .distinct()
+      .stitch()
+    val arrayLP : Array[LabeledPoint] = Array.ofDim[LabeledPoint](tile.rows * tile.cols)
+    for( y <- 0 until tile.rows-1; x <- 0 until tile.cols-1){
+      val bandbuffer = Array.fill(tile.bandCount)(0.0)  // Array[Double]
+      for(bandno <- 0 until tile.bandCount-1){
+        bandbuffer(bandno) = tile.band(bandno).get(x,y)
+      }
+      val bandVector = Vectors.dense(bandbuffer)  // org.apache.spark.mllib.linalg.Vector
+      val classid : Double = 0.0  // ToDo: Apply class id here
+      val labeledPixel = LabeledPoint(classid,bandVector.compressed)
+      arrayLP(y * tile.cols + x) = labeledPixel
+    }
+    val data_temp: RDD[LabeledPoint] = rdd.sparkContext
+      .parallelize(arrayLP)
+      .map(item=>LabeledPoint(item.label, item.features))
+    data_temp
+  }
+
+  def SaveAsLibSVMFile (data : RDD[LabeledPoint], trainingName : String): Unit ={
+    try{
+      val hdfs = org.apache.hadoop.fs.FileSystem.get(data.sparkContext.hadoopConfiguration)
+      if(hdfs.exists(new org.apache.hadoop.fs.Path(trainingName))){
+        try { hdfs.delete(new org.apache.hadoop.fs.Path(trainingName), true)} catch { case _ : Throwable =>  }
+      }
+      MLUtils.saveAsLibSVMFile(data, trainingName)
+    }
+    catch{ case _ : Throwable => }
+  }
+}
