51535e97faaddb173ba02bc19d0a9febee45ec5c
Adrian Klink <Adrian.Klink@eftas.com>
2017-02-15 20:02:21 +0100

    DeleteLayer move SparkContext from apply(..) to main(..)

--- src/main/scala/biggis/landuse/spark/examples/DeleteLayer.scala
+++ src/main/scala/biggis/landuse/spark/examples/DeleteLayer.scala
@@ -9,16 +9,18 @@ object DeleteLayer extends App with LazyLogging {
 
   try {
     val Array(catalogPath, layerName) = args
-    DeleteLayer(layerName)(catalogPath)
+    implicit val sc = Utils.initSparkContext
+    DeleteLayer(layerName)(catalogPath, sc)
+    sc.stop()
   } catch {
     case _: MatchError => println("Run as: /path/to/catalog layerName")
     case e: SparkException => logger error e.getMessage + ". Try to set JVM parmaeter: -Dspark.master=local[*]"
   }
 
-  def apply(layerName: String)(implicit catalogPath: String): Unit = {
+  def apply(layerName: String)(implicit catalogPath: String, sc: SparkContext): Unit = {
     logger info s"Deleting layer $layerName including all zoom levels in catalog $catalogPath ..."
 
-    implicit val sc = Utils.initSparkContext
+    //implicit val sc = Utils.initSparkContext
 
     val hdfsPath = new Path(catalogPath)
     val attributeStore = HadoopAttributeStore(hdfsPath)
