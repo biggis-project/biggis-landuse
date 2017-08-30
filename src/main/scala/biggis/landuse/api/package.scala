868e2d031b2d8433ae800356677a311e39b2420a
Viliam Simko <viliam.simko@gmail.com>
2017-08-31 01:30:33 +0200

    First version of the biggis.landuse.api. This should replace several Utils* in future and automate common tasks. At the moment, there are just a bunch of functions in package.scala

---
+++ src/main/scala/biggis/landuse/api/package.scala
@@ -0,0 +1,98 @@
+package biggis.landuse
+
+import com.typesafe.scalalogging.LazyLogging
+import geotrellis.raster.Tile
+import geotrellis.raster.io.HistogramDoubleFormat
+import geotrellis.spark.LayerId
+import geotrellis.spark.Metadata
+import geotrellis.spark.SpatialKey
+import geotrellis.spark.TileLayerMetadata
+import geotrellis.spark.io.hadoop.HadoopAttributeStore
+import geotrellis.spark.io.hadoop.HadoopLayerDeleter
+import geotrellis.spark.io.hadoop.HadoopLayerWriter
+import geotrellis.spark.io.index.ZCurveKeyIndexMethod
+import geotrellis.spark.io.json.Implicits._
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.fs.RemoteIterator
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+
+package object api extends LazyLogging {
+
+  /**
+    * Converts RemoteIterator from Hadoop to Scala Iterator that provides all the familiar functions such as map,
+    * filter, foreach, etc.
+    *
+    * @param underlying The RemoteIterator that needs to be wrapped
+    * @tparam T Items inside the iterator
+    * @return Standard Scala Iterator
+    */
+  implicit def convertToScalaIterator[T](underlying: RemoteIterator[T]): Iterator[T] = {
+    case class wrapper(underlying: RemoteIterator[T]) extends Iterator[T] {
+      override def hasNext = underlying.hasNext
+
+      override def next = underlying.next
+    }
+    wrapper(underlying)
+  }
+
+  implicit def catalogToStore(catalogPath: String)(implicit sc: SparkContext): HadoopAttributeStore = {
+    val hdfsPath = new Path(catalogPath)
+    HadoopAttributeStore(hdfsPath)
+  }
+
+  def deleteLayerFromCatalog(layerId: LayerId)(implicit catalogPath: String, sc: SparkContext): Unit = {
+    deleteLayerFromCatalog(layerId.name)
+  }
+
+  /**
+    * Unconditionally deletes a raster layer (including all zoom levels) from a geotrellis catalog.
+    * Does not complain if the layer does not exist.
+    */
+  def deleteLayerFromCatalog(layerName: String)(implicit catalogPath: String, sc: SparkContext): Unit = {
+
+    val store = catalogToStore(catalogPath)
+
+    val deleter = HadoopLayerDeleter(store)
+    deleter.attributeStore.layerIds filter (_.name == layerName) foreach deleter.delete
+
+    // try to delete the directory if it still exists
+    // we do this because geotrellis leaves an empty directory behind
+    // we could delete this step once geotrellis implementation is fixed
+    val layerPath = store.rootPath.suffix(s"/$layerName")
+    store.fs.delete(layerPath, true)
+  }
+
+  /**
+    * Unconditionally deletes a single zoom level within a raster layer (inside a geotrellis catalog).
+    * Does not complain if the zoom level does not exist.
+    */
+  def deleteZoomLevelFromLayer(layerName: String, zoomLevel: Int)
+                              (implicit catalogPath: String, sc: SparkContext): Unit = {
+    val deleter = HadoopLayerDeleter(catalogPath)
+
+    deleter.attributeStore.layerIds filter (_.name == layerName) filter (_.zoom == zoomLevel) foreach { layerId =>
+      deleter.delete(layerId)
+      logger debug s"Deleted $layerId"
+    }
+  }
+
+  /**
+    * @param rdd         The RDD representing a processed layer of tiles
+    * @param layerId     layerName and zoom level
+    * @param catalogPath Geotrellis catalog
+    * @param sc          SparkContext
+    */
+  def writeRddToLayer(rdd: RDD[(SpatialKey, Tile)] with Metadata[TileLayerMetadata[SpatialKey]], layerId: LayerId)
+                     (implicit catalogPath: String, sc: SparkContext): Unit = {
+    logger debug s"Writing RDD to layer '${layerId.name}' at zoom level ${layerId.zoom} ..."
+
+    val writer = HadoopLayerWriter(new Path(catalogPath))
+    writer.write(layerId, rdd, ZCurveKeyIndexMethod)
+
+    logger debug s"Writing histogram of layer '${layerId.name}' to attribute store as 'histogramData' for zoom level 0"
+    writer.attributeStore.write(LayerId(layerId.name, 0), "histogramData", rdd.histogram)
+
+    logger debug s"Writing done..."
+  }
+}
